4593aa17c0e8555c7f90db5f4e2ec6f9a10ba36a
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2022-11-15 00:00:05 +0000

    add new occl clothing

--- Contents/mods/OCCL/media/lua/client/occupationalclothing/addons/str.lua
+++ Contents/mods/OCCL/media/lua/client/occupationalclothing/addons/str.lua
@@ -26,9 +26,12 @@ end
 
 function OCCL.STR.addClothes()
     OCCL.UpdateClothing('fireofficer', {
+        Shirt = {'Shirt_Fire_Louisville', 'Shirt_Fire_Supervisor_Louisville', 'Shirt_Fire_Meade', 'Shirt_Fire_Supervisor_Meade', 'Shirt_Fire_Rosewood', 'Shirt_Fire_Supervisor_Rosewood'},
         Tshirt = {'TShirt_Profession_FiremanBlue_Meade', 'TShirt_Profession_FiremanRed_Meade', 'TShirt_Profession_FiremanRed_Meade02', 'TShirt_Profession_FiremanWhite_Meade',
                 'TShirt_Profession_FiremanBlue_Rosewood', 'TShirt_Profession_FiremanRed_Rosewood', 'TShirt_Profession_FiremanRed_Rosewood02', 'TShirt_Profession_FiremanWhite_Rosewood',
-                'TShirt_Profession_FiremanBlue_Louisville', 'TShirt_Profession_FiremanRed_Louisville', 'TShirt_Profession_FiremanRed_Louisville02', 'TShirt_Profession_FiremanWhite_Louisville'},
+                'TShirt_Profession_FiremanBlue_Louisville', 'TShirt_Profession_FiremanRed_Louisville', 'TShirt_Profession_FiremanRed_Louisville02', 'TShirt_Profession_FiremanWhite_Louisville',
+                'TShirt_Fire_Louisville', 'TShirt_Fire_Supervisor_Louisville', 'TShirt_Fire_Meade', 'TShirt_Fire_Supervisor_Meade', 'TShirt_Fire_Rosewood', 'TShirt_Fire_Supervisor_Rosewood',},
+        Pants = {'Trousers_Fire_Formal_Louisville', 'Trousers_Fire_Formal_Meade', 'Trousers_Fire_Formal_Rosewood',}
     }, 'STR')
 
     OCCL.UpdateClothing('policeofficer', {
@@ -50,7 +53,8 @@ function OCCL.STR.addClothes()
     OCCL.UpdateClothing('parkranger', {
         Hat = {'Hat_FederalRanger', 'Hat_StateLawRanger', 'Hat_StateRanger'},
         Shirt = {'Shirt_Federal_Ranger', 'Shirt_State_LawRanger', 'Shirt_State_Ranger'},
-        Tshirt = {'TShirt_Federal_Ranger', 'TShirt_State_LawRanger', 'TShirt_State_Ranger'},
+        Tshirt = {'TShirt_Federal_Ranger', 'TShirt_State_LawRanger', 'TShirt_State_Ranger',
+        'TShirt_Polo_Federal_Ranger', 'TShirt_Polo_State_Ranger', 'TShirt_Polo_State_Law_Ranger'},
         Pants = {'Trousers_FederalRanger', 'Trousers_StateLawRanger', 'Trousers_StateRanger'},
     }, 'STR')
 
@@ -85,6 +89,12 @@ function OCCL.STR.addClothes()
         OCCL.UpdateClothing('policeofficer', {
             TorsoExtra = {'Base.Vest_BulletPolice', 'STR.Vest_BulletPolice_Meade'},
         }, false)
+        OCCL.UpdateClothing('policeofficer', {
+            Hat = {'Hat_Riot_Helmet_Louisville', 'Hat_Riot_Helmet_Grey', 'Hat_Riot_Helmet_Meade', 'Hat_Riot_Helmet_Brown', 'Hat_Riot_Helmet_Brown_Stripe',
+            'Hat_Riot_Helmet_Black', 'Hat_Riot_Helmet_Blue', 'Hat_Riot_Helmet_Blue_Stripe', 'Hat_Riot_Helmet_Navy', 'Hat_Riot_Helmet_Navy_Stripe',
+            'Hat_Riot_Helmet_Brown_Dark', 'Hat_Riot_Helmet_Brown_Dark_Stripe'},
+            Gloves = {'Gloves_Leather_Black', 'Gloves_Leather_Brown'},
+        }, 'STR')
     end
 
     if SandboxVars.OccupationalClothing.WantFireman > 1 then
@@ -99,6 +109,11 @@ function OCCL.STR.addClothes()
                 'Jacket_Fireman_Rosewood_Black', 'Jacket_Fireman_Rosewood_Khaki',
                 'Jacket_Fireman_Louisville_Black', 'Jacket_Fireman_Louisville_Khaki'},
             }, 'STR')
+            if SandboxVars.OccupationalClothing.WantGloves then
+                OCCL.UpdateClothing('fireofficer', {
+                    Gloves = {'Gloves_Fireman_Khaki', 'Gloves_Fireman_Khaki_Simple', 'Gloves_Fireman_Black', 'Gloves_Fireman_Black_Simple'}
+                }, 'STR')
+            end
         end
     end
 
@@ -106,7 +121,8 @@ function OCCL.STR.addClothes()
         OCCL.UpdateClothing('fireofficer', {
             Hat = {'Hat_Fireman_Meade_Black', 'Hat_Fireman_Meade_Red', 'Hat_Fireman_Meade_Yellow',
             'Hat_Fireman_Rosewood_Black', 'Hat_Fireman_Rosewood_Red', 'Hat_Fireman_Rosewood_Yellow',
-            'Hat_Fireman_Louisville_Black', 'Hat_Fireman_Louisville_Red', 'Hat_Fireman_Louisville_Yellow'},
+            'Hat_Fireman_Louisville_Black', 'Hat_Fireman_Louisville_Red', 'Hat_Fireman_Louisville_Yellow',
+            'Hat_Fireman_Louisville_Blue', 'Hat_Fireman_Louisville_White', 'Hat_Fireman_Rosewood_White', 'Hat_Fireman_Meade_White'},
         }, 'STR')
     end
 end
@@ -114,7 +130,6 @@ end
 OCCL.addAdditionalClothing(OCCL.STR.addClothes, OCCL.STR.checkEnabled)
 
 function OCCL.STR.checkNonspecificEnabled()
-    print('checking str nonspecific')
     return (SandboxVars.OccupationalClothing.WantUnemployedNonspecificOutfits and OCCL.STR.checkEnabled())
 end
 
@@ -129,3 +144,43 @@ function OCCL.STR.addNonspecificOutfits()
 end
 
 OCCL.addAdditionalClothing(OCCL.STR.addNonspecificOutfits, OCCL.STR.checkNonspecificEnabled)
+
+function OCCL.STR.checkDressEnabled()
+    return (SandboxVars.OccupationalClothing.WantSTRFormalDress and OCCL.STR.checkEnabled())
+end
+
+function OCCL.STR.addDressOutfits()
+    OCCL.UpdateClothing('fireofficer', {
+        Hat = {'Hat_Fire_Louisville', 'Hat_Fire_Supervisor_Louisville'},
+        Shirt = {'Shirt_Fire_Supervisor_Formal_Louisville'},
+        Tshirt = {'TShirt_Fire_Supervisor_Formal_Louisville'},
+        Gloves = {'Gloves_Dress_White'},
+    }, 'STR')
+    OCCL.UpdateClothing('policeofficer', {
+        Gloves = {'Gloves_Dress_White'},
+    }, 'STR')
+    OCCL.UpdateClothing('parkranger', {
+        Gloves = {'Gloves_Dress_White'},
+    }, 'STR')
+    OCCL.UpdateClothing('EMS', {
+        Hat = {'Hat_EMS_Jefferson', 'Hat_EMS_Louisville', 'Hat_EMS_Meade'},
+        Gloves = {'Gloves_Dress_White'},
+    }, 'STR')
+    if SandboxVars.OccupationalClothing.WantJackets then
+        OCCL.UpdateClothing('fireofficer', {
+            Jacket = {'Jacket_Dress_Fire_Louisville'},
+        }, 'STR')
+        OCCL.UpdateClothing('policeofficer', {
+            Jacket = {'Jacket_Dress_Police_Rosewood', 'Jacket_Dress_Police_Jefferson', 'Jacket_Dress_Police_KSP', 'Jacket_Dress_Police_Louisville',
+            'Jacket_Dress_Police_Meade', 'Jacket_Dress_Police_Muldraugh', 'Jacket_Dress_Police_Riverside', 'Jacket_Dress_Police_WestPoint', 'Jacket_Dress_Sheriff_Jefferson'},
+        }, 'STR')
+        OCCL.UpdateClothing('parkranger', {
+            Jacket = {'Jacket_Dress_Federal_Ranger', 'Jacket_Dress_State_Ranger', 'Jacket_Dress_State_Law_Ranger'},
+        }, 'STR')
+        OCCL.UpdateClothing('EMS', {
+            Jacket = {'Jacket_Dress_EMS_Jefferson', 'Jacket_Dress_EMS_Louisville', 'Jacket_Dress_EMS_Meade'},
+        }, 'STR')
+    end
+end
+
+OCCL.addAdditionalClothing(OCCL.STR.addDressOutfits, OCCL.STR.checkDressEnabled)
--- Contents/mods/OCCL/media/lua/client/suggestClothing_RCPD.lua
+++ Contents/mods/OCCL/media/lua/client/suggestClothing_RCPD.lua
@@ -1,1 +1,2 @@
 -- this file has to remain here, as steam workshop won't delete files that are removed in updates
+return
